aea8a6306b18d60d487910e29e6de91c5b92ae6d
naifamoodi <naifamoodi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-04 13:36:03 +0000

    Version 0.13 of plugin.
    git-svn-id: https://plugins.svn.wordpress.org/fields-framework/tags/0.13@782575 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/media-uploader.js
+++ js/media-uploader.js
@@ -4,58 +4,44 @@
  * Modified by http://www.rhyzz.com
  */
 
-(function($) {
-	var frame;
-
-	$( function() {
-		// Build the choose from library frame.
-		$('.ff-fields').on('click', '.ff_upload_media', function(event) {
-			var $el = $(this);
-
-			event.preventDefault();
-
-			// If the media frame already exists, reopen it.
-			if (frame) {
-				frame.open();
-				return;
-			}
-
-			var library_types = null
-			
-			if($el.data('library')) {
-				library_types = $el.data('library');
-			}
-
-			// Create the media frame.
-			frame = wp.media.frames.custom_upload = wp.media({
-				// Set the title of the modal.
-				title: $el.data('choose'),
-
-				// Tell the modal to show only images.
-				library: {
-					type: library_types
-				},
-
-				// Customize the submit button.
-				button: {
-					// Set the text of the button.
-					text: $el.data('update'),
-					// Tell the button not to close the modal, since we're
-					// going to refresh the page when the image is selected.
-					close: true
+function ff_media_uploader() {
+	(function($) {
+		var frame;
+	
+		$(function() {
+			// Build the choose from library frame.
+			$('.ff-fields').on('click', '.ff_upload_media', function(event) {
+				var $el = $(this);
+	
+				event.preventDefault();
+	
+				if(frame !== undefined) {
+					frame.close();
 				}
-				,multiple: false
+	
+				// Create the media frame.
+				frame = wp.media.frames.custom_upload = wp.media({
+					// Tell the modal to show only images.
+					library: {
+						type: $el.data('library')
+					}
+				});
+	
+				// When an image is selected, run a callback.
+				frame.on('select', function() {
+					var attachment = frame.state().get('selection').first().toJSON();
+	
+					jQuery('#' + $el.data('to')).val(attachment.url);
+				});
+	
+				frame.open();
+	
+				var post_id = $('#post_ID').val();
+	
+				if(post_id !== undefined && frame.uploader.uploader.param('post_id') === undefined) {
+					frame.uploader.uploader.param('post_id', post_id);
+				}
 			});
-
-			// When an image is selected, run a callback.
-			frame.on( 'select', function() {
-				var attachment = frame.state().get('selection').first().toJSON();
-
-				jQuery('#' + $el.data('to')).val(attachment.url);
-
-			});
-
-			frame.open();
 		});
-	});
-}(jQuery));
+	}(jQuery));
+}

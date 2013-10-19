b4740bfbf7730620630c6fcdbe074159f76018fd
naifamoodi <naifamoodi@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-10-19 13:25:26 +0000

    Committing version 0.14.2 of plugin.
    git-svn-id: https://plugins.svn.wordpress.org/fields-framework/trunk@790491 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ js/repeatable-fields.js
@@ -0,0 +1,103 @@
+/*
+ * jQuery Repeatable Fields v1.0
+ * http://www.rhyzz.com/repeatable-fields.html
+ *
+ * Copyright (c) 2013 Rhyzz
+ * License GPL
+*/
+
+(function($) {
+	$.fn.repeatable_fields = function(custom_settings) {
+		var default_settings = {
+			wrapper: '.wrapper',
+			container: '.container',
+			row: '.row',
+			add: '.add',
+			remove: '.remove',
+			move: '.move',
+			template: '.template',
+			is_sortable: true,
+			before_add: null,
+			after_add: after_add,
+			before_remove: null,
+			after_remove: null,
+		}
+
+		var settings = $.extend(default_settings, custom_settings);
+
+		// Initialize all repeatable field wrappers
+		initialize(this);
+
+		function initialize(parent) {
+			$(settings.wrapper, parent).each(function(index, element) {
+				wrapper = this;
+
+				var container = $(wrapper).children(settings.container);
+
+				// Disable all form elements inside the row template
+				$(container).children(settings.template).find(':input').each(function() {
+					jQuery(this).prop('disabled', true);
+				});
+
+				$(wrapper).on('click', settings.add, function(event) {
+					event.stopImmediatePropagation();
+
+					var row_template = $($(container).children(settings.template).clone().removeClass(settings.template.replace('.', ''))[0].outerHTML);
+
+					// Enable all form elements inside the row template
+					jQuery(row_template).find(':input').each(function() {
+						jQuery(this).prop('disabled', false);
+					});
+
+					if(typeof settings.before_add === 'function') {
+						settings.before_add(container);
+					}
+
+					var new_row = $(row_template).show().appendTo(container);
+
+					if(typeof settings.after_add === 'function') {
+						settings.after_add(container, new_row);
+					}
+
+					// The new row might have it's own repeatable field wrappers so initialize them too
+					initialize(new_row);
+				});
+
+				$(wrapper).on('click', settings.remove, function(event) {
+					event.stopImmediatePropagation();
+
+					var row = $(this).parents(settings.row).first();
+
+					if(typeof settings.before_remove === 'function') {
+						settings.before_remove(container, row);
+					}
+
+					row.remove();
+
+					if(typeof settings.after_remove === 'function') {
+						settings.after_remove(container);
+					}
+				});
+
+				if(settings.is_sortable == true && typeof $.ui !== 'undefined' && typeof $.ui.sortable !== 'undefined') {
+					$(wrapper).find(settings.container).sortable({
+						handle: settings.move,
+						row: $(settings.row, '>')
+					});
+				}
+			});
+		}
+
+		function after_add(container, new_row) {
+			var row_count = $(container).children(settings.row).filter(function() {
+				return !jQuery(this).hasClass(settings.template.replace('.', ''));
+			}).length;
+
+			$('*', new_row).each(function() {
+				$.each(this.attributes, function(index, element) {
+					this.value = this.value.replace(/{{row-count-placeholder}}/, row_count - 1);
+				});
+			});
+		}
+	}
+})(jQuery);
